0e57e36db68b2c8dc3eeaf9a86541be6ce258264
kotori5210 <yukari5210am@gmail.com>
2026-08-14 19:38:11 +0900

    Refactor bowling with guard clauses

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -7,35 +7,38 @@ frames = []
 frame = []
 
 scores.each do |score|
-  if frames.size < 9
+  if frames.size >= 9
     frame << score
-    if frame.size == 2 || frame[0] == 10
-      frames << frame
-      frame = []
-    end
-  else
-    frame << score
+    next
+  end
+
+  frame << score
+  if frame.size == 2 || frame[0] == 10
+    frames << frame
+    frame = []
   end
 end
+
 frames << frame unless frame.empty?
 
 total_score = 0
 shot_index = 0
 
 frames.each_with_index do |current_frame, index|
-  if index < 9
-    if current_frame[0] == 10
-      total_score += 10 + scores[shot_index + 1] + scores[shot_index + 2]
-      shot_index += 1
-    elsif current_frame.sum == 10
-      total_score += 10 + scores[shot_index + 2]
-      shot_index += 2
-    else
-      total_score += current_frame.sum
-      shot_index += 2
-    end
+  if index >= 9
+    total_score += current_frame.sum
+    next
+  end
+
+  if current_frame[0] == 10
+    total_score += 10 + scores[shot_index + 1] + scores[shot_index + 2]
+    shot_index += 1
+  elsif current_frame.sum == 10
+    total_score += 10 + scores[shot_index + 2]
+    shot_index += 2
   else
     total_score += current_frame.sum
+    shot_index += 2
   end
 end
 
